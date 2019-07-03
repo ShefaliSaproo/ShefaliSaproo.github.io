09d6fdb82c318f5c2787cddba4f9052301e06b72
shefali <sapru77@gmail.com>
2019-07-03 19:50:23 -0400

    PHP

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <!-- // -->
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-MfvZlkHCEqatNoGiOXveE8FIwMzZg4W85qfrfIFBfYc= sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+<!-- // -->
+    
+    
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body, html {
+  height: 100%;
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.hero-image {
+  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("images/bg1.png");
+  height: 50%;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+}
+
+.hero-text {
+  text-align: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
+}
+
+.hero-text button {
+  border: none;
+  outline: 0;
+  display: inline-block;
+  padding: 10px 25px;
+  color: black;
+  background-color: #ddd;
+  text-align: center;
+  cursor: pointer;
+}
+
+.hero-text button:hover {
+  background-color: #555;
+  color: white;
+}
+    
+.about-us{
+    padding: 25px 50px 75px 100px;
+}
+    
+</style>
+</head>
+<body>
+
+<div class="hero-image">
+  <div class="hero-text">
+    <h1 style="font-size:50px">Welcome to the Burger Shop</h1>
+    <p>Check Out our delicious Burgers!!</p>
+    <a href="store.php"><input type="button" name="store" value="View our menu!" class="btn-primary col-sm-12"></a>
+  </div>
+</div>
+
+    <div class="about-us">
+<h2>About us!</h2>
+    <h3>Long before we opened the doors, we started in our kitchen. We were aiming for nothing short of perfection.</h3>
+    <p>Today, every hand-pressed burger that leaves our kitchen is made from our unique blend of sirloin, round and prime Canadian Beef. It’s authentic, free of preservatives and flame-grilled to juicy perfection <br></p>
+    <h3>We look forward to serve you!!</h3>
+    </div>
+    
+    
+    
+    <div class="container">
+    <hr>
+        <div class="text-center center-block">
+            <p class="txt-railway">Get Connected!!</p>
+            <br />
+                <a href="https://www.facebook.com/bootsnipp"><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a>
+	            <a href="https://twitter.com/bootsnipp"><i id="social-tw" class="fa fa-twitter-square fa-3x social"></i></a>
+	            <a href="https://plus.google.com/+Bootsnipp-page"><i id="social-gp" class="fa fa-google-plus-square fa-3x social"></i></a>
+	            <a href="mailto:bootsnipp@gmail.com"><i id="social-em" class="fa fa-envelope-square fa-3x social"></i></a>
+</div>
+    <hr>
+</div>
+
+  
+
+</body>
+</html>
+
